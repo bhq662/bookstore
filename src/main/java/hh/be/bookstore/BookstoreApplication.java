8ca8857f19c8f1d6c36ed8b323e3bc2461ae11c2
minttu.e.backstrom@gmail.com <minttu.e.backstrom@gmail.com>
2025-09-14 22:36:01 +0300

    c.2 finished

--- src/main/java/hh/be/bookstore/BookstoreApplication.java
+++ src/main/java/hh/be/bookstore/BookstoreApplication.java
@@ -19,13 +19,13 @@ public class BookstoreApplication {
 	public CommandLineRunner demo(BookRepository repository) {
 		return (args) -> {
 			Book b1 = repository
-					.save(new Book("Crime and punishment", "Fyodor Dostoyevsky", 1866L, 9781840224306L, 9.99f));
+					.save(new Book("Crime and punishment", "Fyodor Dostoyevsky", 1866, 9781840224306L, 9.99f));
 			System.out
 					.println("Book id= " + b1.getIsbn() + ", title= " + b1.getTitle() + ", author= " + b1.getAuthor());
-			Book b2 = repository.save(new Book("Foe", "Iain Reid", 2019L, 9781471177989L, 14.99f));
+			Book b2 = repository.save(new Book("Foe", "Iain Reid", 2019, 9781471177989L, 14.99f));
 			System.out
 					.println("Book id= " + b2.getIsbn() + ", title= " + b2.getTitle() + ", author= " + b2.getAuthor());
-			Book b3 = repository.save(new Book("1Q84", "Haruki Murakami", 2012L, 9780099578079L, 22.99f));
+			Book b3 = repository.save(new Book("1Q84", "Haruki Murakami", 2012, 9780099578079L, 22.99f));
 			System.out
 					.println("Book id= " + b3.getIsbn() + ", title= " + b3.getTitle() + ", author= " + b3.getAuthor());
 		};
--- src/main/java/hh/be/bookstore/web/BookController.java
+++ src/main/java/hh/be/bookstore/web/BookController.java
@@ -1,19 +1,30 @@
 package hh.be.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import hh.be.bookstore.domain.Book;
+import hh.be.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
+    @Autowired
+    private BookRepository repository;
 
     @GetMapping("/index")
     public String getBook(@ModelAttribute Book book, Model model) {
         model.addAttribute("book", new Book());
         return "index"; // index.html
+    }
+
+    @GetMapping("/booklist")
+    public String getBookList(@ModelAttribute Book book, Model model) {
+        model.addAttribute("booklist", repository.findAll());
+        return "booklist"; // booklist.html
     }
 
 }
